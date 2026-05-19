3c1221fdf6b7fdc01f14cc8a0e2af35b7dcfae99
Siddharth Adelkar <adelkar@berkeley.edu>
2026-05-19 23:49:26 +0530

    all fields

--- app.py
+++ app.py
@@ -18,7 +18,15 @@ url = "https://www.forbes.com/forbesapi/person/rtb/0/position/true.json"
 # 'familyList', 'interactive', 'selfMadeRank', 'archivedWorth', 'thumbnail', 'squareImage', 'bioSuppress', 
 # 'csfDisplayFields', 'bios', 'abouts', 'philanthropyScore', 'wealthHistory'])
 params = {
-    "fields": "personName,finalWorth,countryOfCitizenship,source,industries,timestamp,date",
+    # "fields": "personName,finalWorth,countryOfCitizenship,source,industries,timestamp,date",
+    "fields": "personName,finalWorth,countryOfCitizenship,source,industries,timestamp,date,"+f"{','.join(['naturalId', 'name', 'year', 'uri', 'rank', 'listUri', 
+'visible', 'position', 'imageExists', 'bio', 'finalWorth', 'person', 'personName', 
+'state', 'city', 'source', 'industries', 
+'countryOfCitizenship', 'timestamp', 'version', 
+'selfMade', 'gender', 'birthDate', 'lastName', 'financialAssets', 
+'date', 'wealthList', 'estWorthPrev', 'privateAssetsWorth', 
+'familyList', 'interactive', 'selfMadeRank', 'archivedWorth', 'thumbnail', 'squareImage', 'bioSuppress', 
+'csfDisplayFields', 'bios', 'abouts', 'philanthropyScore', 'wealthHistory'])}",
     "limit": 4000
 }
 
@@ -50,7 +58,7 @@ india_df['time'] = df["timestamp"].apply(
     )
 )
 
-india_df['date'] = df["date"].apply(
+india_df['date-correct'] = df["date"].apply(
     lambda ts: (
         datetime.fromtimestamp(ts / 1000, tz=timezone.utc)
         .astimezone(ZoneInfo("Asia/Kolkata")).replace(tzinfo=None)
