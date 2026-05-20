4df24386741e99c09c320913475063fead530130
Siddharth Adelkar <adelkar@berkeley.edu>
2026-05-20 22:11:24 +0530

    dictionary debug7

--- pages/history.py
+++ pages/history.py
@@ -70,7 +70,6 @@ for _, row in india_df.head(TOP_N).iterrows():
     values = []
 
     for item in wealth_history.items():
-        st.write(item[1])
         points = item[1]
         for point in points:
             # Adjust keys if needed after debugging
